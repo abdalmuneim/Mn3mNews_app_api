8af3d679e67898ae3715cc91faa082492e44c122
Abdalmuneim <mo.m.eg21@gmail.com>
2021-09-01 21:17:10 -0700

    mn3mNews_app_api

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,31 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
 import 'package:flutter/material.dart';
-import '../views/first_open.dart';
-import '../views/homepage.dart';
-
+import '../screens_builder/first_open.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../screens_builder/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences preferences = await SharedPreferences.getInstance();
   bool decision = preferences.getBool('view');
-  Widget _screens =
-      (decision == false || decision == null) ? FirstOpen() : HomePage();
-
-  runApp(MyApp(screens: _screens));
+  Widget screens = (decision == false || decision == null) ? FirstOpen() : HomePage();
+  runApp(MyApp(qScreens: screens));
 }
 
 class MyApp extends StatelessWidget {
-  final Widget screens;
+  final Widget qScreens;
 
-  const MyApp({this.screens});
+  const MyApp({this.qScreens});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
       // home: screens,
       routes: {
-        '/': (context) => screens,
+        '/': (context) => qScreens,
         HomePage.routName: (context) => HomePage(),
       },
     );
